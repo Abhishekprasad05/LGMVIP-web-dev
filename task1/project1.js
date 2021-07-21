6b21a6d799e74c7126655014673f4dd2349a4f94
Abhishekprasad05 <81014005+Abhishekprasad05@users.noreply.github.com>
2021-07-21 13:05:27 +0530

    Add files via upload

---
+++ task1/project1.js
@@ -0,0 +1,11 @@
+
+
+       function funVideo(video)
+       {
+           document.getElementById("slider").src=video;
+       }
+
+function Picalbum(image)
+ {
+ document.getElementById("alimg").src=image;
+}
